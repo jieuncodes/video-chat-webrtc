93044c5a031983cfe0b4b6f8e236aee8816b29eb
youredith <110220308+jieuncodes@users.noreply.github.com>
2023-10-17 17:03:13 +0900

    chore: setup UI libraries

---
+++ client/tailwind.config.js
@@ -0,0 +1,25 @@
+const { nextui } = require("@nextui-org/react");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      backgroundImage: {
+        atlas: "url('/images/ship_inside.webp')",
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+};
