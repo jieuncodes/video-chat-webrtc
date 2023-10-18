a07a2aa73d60f2784cfd17f5072a262df5ab51cf
youredith <110220308+jieuncodes@users.noreply.github.com>
2023-10-18 22:24:14 +0900

    feat: socket.js in vite

--- client/src/components/CreateRoom.tsx
+++ client/src/components/CreateRoom.tsx
@@ -1,8 +1,20 @@
 import { Input } from "@nextui-org/react";
+import { useState } from "react";
+import { socket } from "../socket";
+
 function CreateRoom() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+
+  const handleRoomSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const inputElement = form.elements[0] as HTMLInputElement;
+    console.log(inputElement.value);
+  };
+
   return (
     <div className="w-96 mb-28">
-      <form action="">
+      <form onSubmit={handleRoomSubmit}>
         <h1 className="text-2xl text-white font-bold mb-6">
           Name of the room is...
         </h1>
@@ -16,7 +28,7 @@ function CreateRoom() {
             isRequired
             className="text-white"
           />
-        </div>{" "}
+        </div>
       </form>
     </div>
   );
--- client/src/components/chat/ChatForm.tsx
+++ client/src/components/chat/ChatForm.tsx
@@ -1,23 +1,10 @@
 import { Button } from "@nextui-org/react";
 import { makeMessage } from "../../helpers";
-import useSocket from "../../hooks/useSocket";
 import { Icons } from "../Icons";
 import { Form, FormBox, Input } from "./../../styles/Chat";
 
 function ChatForm() {
-  const { currSocket } = useSocket(`ws://${import.meta.env.VITE_SERVER_URL}`);
-
-  const handleSubmitChat = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const inputField = e.currentTarget.elements[0] as HTMLInputElement;
-    const messageStr = inputField.value.toString();
-    if (currSocket && messageStr.trim() !== "") {
-      currSocket.send(
-        makeMessage({ type: "new_message", payload: messageStr })
-      );
-      inputField.value = "";
-    }
-  };
+  const handleSubmitChat = (e: React.FormEvent<HTMLFormElement>) => {};
 
   return (
     <FormBox>
